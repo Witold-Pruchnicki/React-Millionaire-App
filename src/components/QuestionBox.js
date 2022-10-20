7c5bacb6ed742c179e5e53975567ece78f5c3f69
Witold Pruchnicki <witekpruchnicki@gmail.com>
2022-10-20 14:52:59 +0200

    add new functionalities to questionBox

--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -1,12 +1,46 @@
-const QuestionBox = () => {
+import { useEffect, useState } from "react";
+
+const QuestionBox = ({
+  data,
+  questionNumber,
+  setQuestionNumber,
+  setTimeOut,
+  setStop,
+}) => {
+  const [question, setQuestion] = useState(null);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [className, setClassName] = useState("answer");
+  useEffect(() => {
+    setQuestion(data[questionNumber - 1]);
+  }, [data, questionNumber]);
+
+  const delay = (duration, callback) => {
+    setTimeOut(() => {
+      callback();
+    },duration);
+  };
+
+  const handleClick = (answer) => {
+    setSelectedAnswer(answer);
+    setClassName("answer active");
+    delay(3000,()=> setClassName(answer.correct ? "answer correct" : "answer incorrect");)
+    setTimeout(() => {
+      setClassName(answer.correct ? "answer correct" : "answer incorrect");
+    }, 3000);
+  };
   return (
     <div className="questionBox">
-      <div className="question">Which programming language is the best?</div>
+      <div className="question">{question?.question}</div>
       <div className="answers">
-        <div className="answer">JavaScript</div>
-        <div className="answer">Java</div>
-        <div className="answer">Python</div>
-        <div className="answer">C++</div>
+        {question?.answers.map((answer) => (
+          <div
+            className={selectedAnswer === answer ? className : " answer"}
+            key={answer.text}
+            onClick={() => handleClick(answer)}
+          >
+            {answer.text}
+          </div>
+        ))}
       </div>
     </div>
   );
